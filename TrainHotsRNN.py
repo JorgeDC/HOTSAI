0ed820dfc9c9eda8a02ef81c54630528300fbabe
JorgeDC <jorgedecorte@gmail.com>
2017-04-30 18:54:56 +0200

    do sigmoid, but not better

--- TrainHotsRNN.py
+++ TrainHotsRNN.py
@@ -14,7 +14,7 @@ lstm_size = 64
 lstm_layers = 1
 embed_size = 30
 
-number_of_labels = 2
+number_of_labels = 1
 
 standard_deviation = 1 / np.sqrt(features_count)
 print(standard_deviation)
@@ -34,23 +34,25 @@ hots_all = np.array(pd.read_csv(data_all))
 sys.stdout.write("\rShuffling data")
 np.random.shuffle(hots_all)
 
-hots_results = np.array(hots_all[:,[features_count, features_count+1]])
+hots_results = np.array(hots_all[:,features_count])
 hots_features = np.array(hots_all[:,:features_count])
 
-num_rows, num_cols = hots_results.shape
+print(hots_results.shape)
+
+num_rows, num_cols = hots_features.shape
 
 train_set_count = math.floor((num_rows / 10) * 8)
 val_test_set_count = math.floor((num_rows / 10) * 1)
 
 train_x, val_x, test_x = hots_features[:train_set_count,:], hots_features[train_set_count:train_set_count+val_test_set_count,:], hots_features[train_set_count+val_test_set_count:,:]
-train_y, val_y, test_y = hots_results[:train_set_count,:], hots_results[train_set_count:train_set_count+val_test_set_count,:], hots_results[train_set_count+val_test_set_count:,:]
+train_y, val_y, test_y = hots_results[:train_set_count], hots_results[train_set_count:train_set_count+val_test_set_count], hots_results[train_set_count+val_test_set_count:]
 
-
+print(train_y.shape)
 features = tf.placeholder(tf.int32, [None, features_count], name="features")
-labels = tf.placeholder(tf.int32, [None, 2], name="labels")
+labels = tf.placeholder(tf.int32, [None, None], name="labels")
 keep_prob = tf.placeholder(tf.float32, shape=(), name="keep_prob")
-
 train_feed_dict = {features: train_x, labels: train_y, keep_prob:dropout_keep_prob}
+
 valid_feed_dict = {features: val_x, labels: val_y, keep_prob:1.0}
 test_feed_dict = {features: test_x, labels: test_y, keep_prob:1.0}
 
@@ -76,24 +78,13 @@ embed = tf.nn.embedding_lookup(embedding, features)
 
 outputs, final_state = tf.nn.dynamic_rnn(cell, embed, initial_state=initial_state)
 
-#output layer
-weights_prediction = tf.Variable(tf.truncated_normal((lstm_size, number_of_labels), mean=0.0, stddev=standard_deviation))
-biases_prediction = tf.Variable(tf.zeros(number_of_labels))
+predictions = tf.contrib.layers.fully_connected(outputs[:, -1], 1, activation_fn=tf.sigmoid)
+cost = tf.losses.mean_squared_error(labels, predictions)
 
-logits_prediction = tf.matmul(outputs[:, -1], weights_prediction) + biases_prediction
-prediction = tf.nn.softmax(logits_prediction)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
-# Name prediction Tensor, so that is can be loaded from disk after training
-prediction = tf.identity(prediction, name='prediction')
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels, name="cross_entropy")
-cost = tf.reduce_mean(cross_entropy)
-
-# Determine if the predictions are correct
-is_correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1))
-# Calculate the accuracy of the predictions
-accuracy = tf.reduce_mean(tf.cast(is_correct_prediction, tf.float32), name='accuracy')
-
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+correct_pred = tf.equal(tf.cast(tf.round(predictions), tf.int32), labels)
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 # The accuracy measured against the validation set
 validation_accuracy = 0.0
@@ -112,6 +103,7 @@ def getBatches(batch_x, batch_y, b_size=100):
     number_of_batches = np.floor(number_features/b_size).astype(np.int)
     print(number_of_batches)
     # The training cycle
+    print(batch_y.shape)
     for batch_i in range(number_of_batches):
         # Get a batch of training features and labels
         batch_start = batch_i * b_size
@@ -149,7 +141,7 @@ with tf.Session() as session:
             #print(x.shape)
             #print(y.shape)
             #print(dropout_keep_prob)
-            loss, state, _ = session.run([cost, final_state, optimizer], feed_dict={features: x, labels: y, keep_prob:dropout_keep_prob})
+            loss, state, _ = session.run([cost, final_state, optimizer], feed_dict={features: x, labels: y[:,None], keep_prob:dropout_keep_prob})
             #print(loss)
 
             if iteration % 500 == 0:
@@ -161,7 +153,7 @@ with tf.Session() as session:
                 for x_val, y_val in getBatches(val_x, val_y, batch_size):
                     #print(x_val.shape)
                     #print(y_val.shape)
-                    feed = {features: x_val, labels: y_val, keep_prob: 1.0, initial_state:val_state}
+                    feed = {features: x_val, labels: y_val[:,None], keep_prob: 1.0, initial_state:val_state}
                     batch_acc, val_state = session.run([accuracy, final_state], feed_dict=feed)
                     # print(batch_acc)
                     val_acc.append(batch_acc)
